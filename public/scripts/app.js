3246eec240c2153f5c9202846ce04679c5fa6396
jvaz11 <juanvme123@gmail.com>
2016-03-17 17:55:45 -0700

    Changed interval

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,7 +28,7 @@ app.controller('MainCtrl', function($scope, $timeout, $firebaseArray, GradientSe
         return $scope.currentIndex === index;
     }
 
-    var INTERVAL = 50000;
+    var INTERVAL = 5000;
 
     function nextSlide() {
         $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
